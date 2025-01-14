60c2eb32578848aee233d0045d70376ee4f96b53
João <eujoaovictor0707@gmail.com>
2025-01-14 17:23:07 -0300

    Criando rotas crud para gerenciamento de tarefas

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
-from fast_zero.routers import auth, users
+from fast_zero.routers import auth, todo, users
 from fast_zero.schemas import Message
 
 app = FastAPI(docs_url="/docs", redoc_url="/redoc")
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todo.router)
+
 
 database = []  # meu fakedb temporario
 
--- fast_zero/fast_zero/models.py
+++ fast_zero/fast_zero/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from enum import Enum
 
-from sqlalchemy import func
+from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, registry
 
 table_registy = registry()
@@ -16,3 +17,23 @@ class User:
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
+
+
+class TodoState(str, Enum):
+    draft = "draft"
+    todo = "todo"
+    doing = "doing"
+    done = "done"
+    trash = "trash"
+
+
+@table_registy.mapped_as_dataclass
+class Todo:
+    __tablename__ = "todos"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    state: Mapped[TodoState]
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
